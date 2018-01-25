1e709429942f68f3fd472f7575e5d175c123d9fe
tiananwei <awei.tian@qq.com>
2018-01-25 09:48:21 +0800

    test in php 5.3

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 使用 composer 命令进行安装或下载源代码使用。
 >composer require aweitian/session
 >
-
+### 测试
+运行 `php test` 来测试
---
+++ tests/SessionTest.php
@@ -0,0 +1,16 @@
+<?php
+
+class SessionTest
+{
+    public function testConfig()
+    {
+        $test = new \Aw\Session();
+        $test->setSavePath(__DIR__.'/session_save_dir');
+        $test->start();
+        $test->set('c.d','foo');
+        var_dump('foo' == $test->get('c.d'));
+        $test->set('b.b','cc');
+        var_dump($_SESSION['b']['b'] == 'cc');
+    }
+}
+
